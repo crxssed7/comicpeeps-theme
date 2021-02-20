546517dfcc203465cdc050cc7d79a46f36578abe
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-02-20 14:40:31 +0000

    update: tried js and failed miserably

---
+++ themeScript.js
@@ -0,0 +1,19 @@
+// Put any javascript needed by your theme here
+//var images = ['comic-bg.png'];
+//console.log(images[Math.floor(Math.random() * images.length)]);
+//// $('body').css({'background-image': 'url(' + images[Math.floor(Math.random() * images.length)] + ')'});
+//// document.getElementById("p2").style.color = "blue";
+////document.getElementsByName("body").entries.style.css({'background-image': 'url(' + images[Math.floor(Math.random() * images.length)] + ')'})
+//
+////var list = document.getElementsByTagName("UL")[0];
+////list.getElementsByTagName("LI")[0].innerHTML = "Milk";
+//
+//setTimeout(MyFunction,1000);
+//
+//function MyFunction(){
+//    console.log("url('" + images[Math.floor(Math.random() * images.length)] + "')");
+//    document.body.style.backgroundImage = "url(" + images[Math.floor(Math.random() * images.length)] + ")"
+//    //document.getElementsByTagName("BODY")[0]
+//}
+//
+//
